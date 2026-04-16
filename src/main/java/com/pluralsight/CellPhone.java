007b5c3f6e55f760085bb585ff0f12d3e9850ee4
pmvroque15 <pmvroque@gmail.com>
2026-04-15 23:08:39 -0400

    Adde getters and setters to

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -1,4 +1,40 @@
 package com.pluralsight;
 
 public class CellPhone {
+    String serialNumber = "0";
+    String model = "";
+    String phoneNumber = "";
+    String owner = "";
+    static void main(String[] args) {
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+    void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+    void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getOwner() {
+        return serialNumber;
+    }
+
+    void setOwner(String owner) {
+        this.owner = owner;
+    }
 }
+
